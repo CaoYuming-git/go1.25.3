b2a5bacfcb00ee97c3b5b16acadc53ccbe543680
caoyu <1372157392@qq.com>
2026-03-04 23:47:04 +0800

    非空接口注释

--- src/internal/abi/iface.go
+++ src/internal/abi/iface.go
@@ -6,16 +6,18 @@ package abi
 
 import "unsafe"
 
-// The first word of every non-empty interface type contains an *ITab.
+// ITab The first word of every non-empty interface type contains an *ITab.
 // It records the underlying concrete type (Type), the interface type it
 // is implementing (Inter), and some ancillary information.
 //
 // allocated in non-garbage-collected memory
+// 非空接口的底层结构
 type ITab struct {
 	Inter *InterfaceType
 	Type  *Type
 	Hash  uint32 // copy of Type.Hash. Used for type switches.
-	//接口的方法表数组，每个元素是指向方法地址的指针，数组大小是动态的
+	//接口的方法表数组，这个可以在动态派发的时候快速找到方法地址，而不用去Type中去找了，
+	//每个元素是指向方法地址的指针，方法数组大小实际是动态的，不只是1，todo会根据Inter中的方法数?，在Fun[0]后面加上其余的的方法
 	Fun [1]uintptr // variable sized. fun[0]==0 means Type does not implement Inter.
 }
 
